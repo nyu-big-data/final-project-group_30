e54d0d354db818f9fa549a266fd848aec958c4e9
SaiKiran_CVN <saikirancvn@gmail.com>
2022-05-08 10:34:31 -0400

    Modify ALS to keep track of history

--- src/als.py
+++ src/als.py
@@ -1,93 +1,104 @@
-
 #Use getpass to obtain user netID
 import getpass
 
-# And pyspark.sql to get the spark session
 from pyspark.sql import SparkSession
-#Importing for groupby
-# from pyspark.sql import functions as F
-from pyspark.sql.functions import *
-from pyspark.sql import DataFrameNaFunctions as DFna
-from pyspark.sql.functions import udf, col, when
-import matplotlib.pyplot as plt
-import pyspark as ps
-import os, sys, requests, json
+from pyspark.mllib.evaluation import  RankingMetrics
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.recommendation import ALS, ALSModel
-from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
-from pyspark.ml import Pipeline
-from pyspark.sql import Row
-import numpy as np
-import math
+from pyspark.ml.recommendation import ALS
+from time import time 
+from pyspark import SparkContext
+from pyspark.sql.functions  import collect_list
+
+def als_grid_search_ranking(train,validation,maxIters,regParams, ranks,spark):
+    '''
+        train: train set
+        val: validation set 
+        maxIters: list of maxIter
+        regParams: list of regParams
+        ranks: list ofranks
+        spark: Spark Object
+    '''
+    models = {} # dict of models_parameter:ALS_obj
+    precision_at_k_scores = {} 
+    maps ={} #mean avearage precision
+    NDCGs = {} 
+    times = {} #time to run differrent models 
+    rmses = {}
+
+    sc = SparkContext.getOrCreate()
+    validation.createOrReplaceTempView('validation')
+
+    # grid-search
+    for r in ranks:
+        for Iter in maxIters:
+            for reg in regParams:
+                st = time()
+                # initialize and train model 
+                model = ALS(rank = r,maxIter=Iter, regParam=reg,userCol='userId',\
+                            itemCol='movieId',ratingCol='rating',seed = 5,coldStartStrategy='drop',nonnegative=True)
+                model = model.fit(train)
+                models[(r,Iter,reg)] = model
+                    
+                #RMSE
+                evaluator=RegressionEvaluator(metricName="rmse",labelCol="rating",predictionCol="prediction")
+                predictions=model.transform(validation)
+                rmse=evaluator.evaluate(predictions)
+
+
+                # evaluate on validation 
+                preds = model.recommendForAllUsers(500)
+                preds.createOrReplaceTempView('preds')
+                
+                val = spark.sql('SELECT userId, movieId FROM validation SORT BY rating DESC')
+                val = val.groupBy('userId').agg(collect_list('movieId').alias('movieId_val'))
+                val.createOrReplaceTempView('val')
+                predAndTruth = spark.sql('SELECT preds.recommendations, val.movieId_val FROM val join preds on preds.userId = val.userId')
+                predAndTruth = predAndTruth.collect()
+                final_predAndTruth = []
+                for item in predAndTruth:
+                    truth = item[1]
+                    pred = [i.movieId for i in item[0]]
+                    final_predAndTruth += [(pred,truth)]
+                    
+                    
+                final_predAndTruth =  sc.parallelize(final_predAndTruth)
+                
+                ranking_obj = RankingMetrics(final_predAndTruth)
+                precision_at_k_scores[(r,Iter,reg)] = ranking_obj.precisionAt(500)
+                maps[(r,Iter,reg)] = ranking_obj.meanAveragePrecision
+                NDCGs[(r,Iter,reg)] = ranking_obj.ndcgAt(500)
+                rmses[(r,Iter,reg)] = rmse
+                times[(r,Iter,reg)] = round(time() - st,5)
+                print('-----------------------------------------------------------------------------')
+                print('For Model with maxIter = {}, reg = {}, rank = {}'.format(Iter,reg,r))
+                print('Precision:',precision_at_k_scores[(r,Iter,reg)],'MAP:',maps[(r,Iter,reg)],'NDCGs:',NDCGs[(r,Iter,reg)],'RMSE:',rmse,'Time Taken:',times[(r,Iter,reg)])
+                print('-----------------------------------------------------------------------------')
+    return models, precision_at_k_scores,maps, NDCGs,rmses,times
+
+
+
+
+
 
 def main(spark, netID):
+    train_ratings_small = spark.read.parquet(f'hdfs:/user/{netID}/ml-latest-small/train_ratings_small.parquet')
+    val_ratings_small = spark.read.parquet(f'hdfs:/user/{netID}/ml-latest-small/val_ratings_small.parquet')
+    maxIters = [5,10,15]
+    regParams = [0.1, 0.01]
+    ranks = [4,5,6,7,8,9,10,11,12]
+    models, precision_at_k_scores,maps, NDCGs,rmses,times = als_grid_search_ranking(train_ratings_small,val_ratings_small,maxIters,regParams, ranks,spark)
 
-	ratings = spark.read.csv(f'hdfs:/user/{netID}/ml-latest-small/ratings.csv', header=True,schema='userId INT, movieId INT, rating FLOAT, timestamp BIGINT')
-
-	train_ratings_small = spark.read.parquet(f'hdfs:/user/{netID}/ml-latest-small/train_ratings_small.parquet')
-	val_ratings_small = spark.read.parquet(f'hdfs:/user/{netID}/ml-latest-small/val_ratings_small.parquet')
-	test_ratings_small = spark.read.parquet(f'hdfs:/user/{netID}/ml-latest-small/test_ratings_small.parquet')
-
-	#Check if they are disjoint
-	print('check disjoint')
-	val_ratings_small.join(test_ratings_small,'userId').show()
-
-
-	movies = spark.read.csv(f'hdfs:/user/{netID}/ml-latest-small/movies.csv',header=True, schema='movieId INT, title string, genres string')
-
-	LOAD = True
-
-	if not LOAD:
-		seed = 5
-		iterations = 10
-		regularization_parameter = 0.1
-		rank = 4
-
-		als = ALS(maxIter=iterations, regParam=regularization_parameter, rank=rank, userCol="userId", itemCol="movieId", ratingCol="rating",seed=seed, nonnegative = True,coldStartStrategy="drop")
-		paramGrid = ParamGridBuilder() \
-	    .addGrid(als.regParam, [0.1, 0.01]) \
-	    .addGrid(als.rank, [4,5,6,7,8,9,10,11,12]) \
-	    .build()
-		evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
-		crossval = CrossValidator(estimator=als, \
-	        estimatorParamMaps=paramGrid, \
-	        evaluator=evaluator, \
-	        numFolds=5)
-		cvModel = crossval.fit(train_ratings_small)
-
-		#Extract best model from the cv model above
-		best_model = cvModel.bestModel
-
-
-		cvModel_pred = best_model.transform(val_ratings_small) # Predict
-		cvModel_pred.show(n=10) # Show 10 predictions
-		cvModel_pred.join(movies,'movieId').select('userId','title','genres','prediction').show(5) # Show along with movie name
-		cvModel_pred = cvModel_pred.filter(col('prediction') != np.nan) # New Predictions
-		rmse = evaluator.evaluate(cvModel_pred)
-
-		print("the rmse for optimal grid parameters with cross validation is: {}".format(rmse))
-
-		print("**Best Model**")
-		# Print "Rank"
-		print("  Rank:", best_model._java_obj.parent().getRank())
-		# Print "MaxIter"
-		print("  MaxIter:", best_model._java_obj.parent().getMaxIter())
-		# Print "RegParam"
-		print("  RegParam:", best_model._java_obj.parent().getRegParam())
-
-
-		#Save
-		best_model.write().overwrite().save(f'hdfs:/user/{netID}/ml-latest-small/train_ratings_small/als_model')
-	else:
-		best_model = ALSModel.load(f'hdfs:/user/{netID}/ml-latest-small/train_ratings_small/als_model')
-
-	#100th User’s ALS Recommendations
-	nrecommendations = best_model.recommendForAllUsers(5)
-	nrecommendations = nrecommendations.withColumn("rec_exp", explode("recommendations")).select('userId', col("rec_exp.movieId"), col("rec_exp.rating"))
-	nrecommendations.join(movies, on='movieId').filter('userId = 100').show(truncate=False)
-
-	#100th User’s Actual Preference
-	ratings.join(movies, on='movieId').filter('userId = 100').sort('rating', ascending=False).limit(10).show(truncate=False)
+    print('**********************************************************************************************')
+    print(precision_at_k_scores)
+    print('**********************************************************************************************')
+    print(maps)
+    print('**********************************************************************************************')
+    print(NDCGs)
+    print('**********************************************************************************************')
+    print(rmses)
+    print('**********************************************************************************************')
+    print(times)
+    print('**********************************************************************************************')
 
 if __name__ == '__main__':
 
@@ -99,3 +110,5 @@ if __name__ == '__main__':
 
     # Call our main routine
     main(spark, netID)
+
+
